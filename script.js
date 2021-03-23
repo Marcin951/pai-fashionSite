c373224d69671e45db9fedbf918e8a042b0a19a4
Marcin951 <31135444+Marcin951@users.noreply.github.com>
2021-03-23 17:45:50 +0100

    slider feature is added

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-new Glide('.glide', {
-    autoplay: 1000,
-    hoverpause: true,
+new Glide('.glide').mount()
 
-}).mount();
+document.getElementById("button1").onclick = function() { myFunction("button1") };
+document.getElementById("button2").onclick = function() { myFunction("button2") };
+document.getElementById("button3").onclick = function() { myFunction("button3") };
+document.getElementById("button4").onclick = function() { myFunction("button4") };
+var lastClickedButton = document.getElementById("button1")
+lastClickedButton.style.color = "red"
+
+function myFunction(id) {
+    if (lastClickedButton != null) {
+        lastClickedButton.style.color = "grey"
+    }
+    lastClickedButton = document.getElementById(id)
+    lastClickedButton.style.color = "red"
+}
